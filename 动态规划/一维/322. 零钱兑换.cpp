1a68c8890ae10230391c1cb21785bc3820b1b0c2
Don't bully Miyu <141989008+WhereIsYuuka@users.noreply.github.com>
2024-05-19 20:58:19 +0800

    2024/5/19

---
+++ 动态规划/一维/322. 零钱兑换.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> res(amount + 1, INT_MAX);
+        res[0] = 0;
+        for(int i = 1; i <= amount; i++)
+            for(int j = 0; j < coins.size(); j++)
+                if(i - coins[j] >= 0 && res[i - coins[j]] != INT_MAX)
+                    res[i] = min(res[i], res[i - coins[j]] + 1);
+                    
+        return res[amount] == INT_MAX ? -1 : res[amount];
+    }
+};
