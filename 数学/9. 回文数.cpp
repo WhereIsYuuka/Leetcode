6ee8dc14a62a99f52c5fa52a996cb1c9bd7dd69b
Don't bully Miyu <141989008+WhereIsYuuka@users.noreply.github.com>
2024-05-20 13:35:39 +0800

    2024/5/20

---
+++ 数学/9. 回文数.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+        long long res = 0, tmp = x;
+        while (tmp) {
+            res = res * 10 + tmp % 10;
+            tmp /= 10;
+        }
+        return res == x;
+    }
+};
