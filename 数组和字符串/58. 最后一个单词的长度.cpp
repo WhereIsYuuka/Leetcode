b47810388dbd0f126f4ed87f57b1a0d590dbcd74
Don't bully Miyu <141989008+WhereIsYuuka@users.noreply.github.com>
2024-05-30 15:24:05 +0800

    2024/5/30

---
+++ 数组和字符串/58. 最后一个单词的长度.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int len = s.size();
+        int res = 0;
+        // 从后往前遍历
+        for(int i = len - 1; i >= 0; i--)
+        {
+            if(s[i] == ' ')
+            {
+                // 如果res为0，说明前面有空格，直接跳过
+                if(res == 0);
+                    continue;
+                // 否则，说明遇到了第一个单词，直接返回
+                break;
+            }
+            res++;
+        }
+        return res;
+    }
+};
