31a0f81cd715032bdffcbf091b0cd89f1864a7e6
Don't bully Miyu <141989008+WhereIsYuuka@users.noreply.github.com>
2024-07-20 22:57:04 +0800

    2024/7/20

---
+++ 动态规划/多维/5. 最长回文子串.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        string res = "";
+        int n = s.length();
+
+        for(int i = 0; i < n; i++)
+        {
+            //奇数
+            int l = i-1, r = i+1;
+            while(l >= 0 && r < n && s[l] == s[r])
+                l--, r++;
+            if(res.length() < r-l-1)
+                res = s.substr(l+1, r-l-1);
+            //偶数
+            l = i, r = i+1;
+            while(l >= 0 && r < n && s[l] == s[r])
+                l--, r++;
+            if(res.length() < r-l-1)
+                res = s.substr(l+1, r-l-1);
+        }
+        return res;
+    }
+};
