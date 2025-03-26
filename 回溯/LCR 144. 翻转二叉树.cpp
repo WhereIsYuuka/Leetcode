963a01025426a0c5b3a0cf546ae531eb2c828ee4
Don't bully Miyu <141989008+WhereIsYuuka@users.noreply.github.com>
2025-03-26 14:47:29 +0800

    2025/3/26

---
+++ 回溯/LCR 144. 翻转二叉树.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    public:
+        TreeNode* flipTree(TreeNode* root) {
+            if(root == nullptr)
+                return nullptr;
+            TreeNode* tmp = root->left;
+            root->left = flipTree(root->right);
+            root->right = flipTree(tmp);
+            return root;
+        }
+    };
