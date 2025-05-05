526dd4017421bcfe2365a03db961a9c96ec0564c
Don't bully Miyu <141989008+WhereIsYuuka@users.noreply.github.com>
2025-05-05 15:31:41 +0800

    2025/5/5 LCR 134. Pow(x, n)

---
+++ 分治/LCR 134. Pow(x, n).cpp
@@ -0,0 +1,14 @@
+class Solution {
+    public:
+        double myPow(double x, long n) {
+            if(n == 0 || x == 0)
+                return 1;
+            if(n < 0)
+                return 1.0 / myPow(x, -n);
+            double half = myPow(x, n/2);
+            if(n % 2 == 0)
+                return half * half;
+            else
+                return half * half * x;
+        }
+    };
