b26e362db251e6c69d5a6bde06df1b073d7f99a6
Don't bully Miyu <141989008+WhereIsYuuka@users.noreply.github.com>
2025-06-30 15:15:38 +0800

    2025/6/30 114. 二叉树展开为链表

--- 二叉树/二叉树/114. 二叉树展开为链表.cpp
+++ 二叉树/二叉树/114. 二叉树展开为链表.cpp
@@ -26,3 +26,39 @@ public:
         flatten(right);
     }
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+// class Solution {
+// public:
+//     vector<TreeNode*> trees;
+//     void flatten(TreeNode* root) {
+//         if(!root)
+//             return;
+            
+//         dfs(root);
+//         for(int i = 0; i < trees.size()-1; i++)
+//         {
+//             trees[i]->left = nullptr;
+//             trees[i]->right = trees[i+1];
+//         }
+//     }
+
+//     void dfs(TreeNode* root)
+//     {
+//         if(!root)
+//             return;
+//         trees.emplace_back(root);
+//         dfs(root->left);
+//         dfs(root->right);
+//     }
+// };
