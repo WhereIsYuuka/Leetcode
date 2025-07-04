42f7a4ceabd63a3df0476825ec8d95070b0956a1
Don't bully Miyu <141989008+WhereIsYuuka@users.noreply.github.com>
2025-07-04 12:10:41 +0800

    2025/7/4 155. 最小栈

--- 栈/155. 最小栈.cpp
+++ 栈/155. 最小栈.cpp
@@ -61,3 +61,36 @@ public:
 //             return INT_MAX;
 //     }
 // };
+
+// class MinStack {
+// public:
+//     stack<pair<int, int>> stk;
+//     MinStack() {
+//         stk.emplace(0, INT_MAX);
+//     }
+    
+//     void push(int val) {
+//         stk.emplace(val, min(getMin(), val));
+//     }
+    
+//     void pop() {
+//         stk.pop();
+//     }
+    
+//     int top() {
+//         return stk.top().first;
+//     }
+    
+//     int getMin() {
+//         return stk.top().second;
+//     }
+// };
+
+// /**
+//  * Your MinStack object will be instantiated and called as such:
+//  * MinStack* obj = new MinStack();
+//  * obj->push(val);
+//  * obj->pop();
+//  * int param_3 = obj->top();
+//  * int param_4 = obj->getMin();
+//  */
