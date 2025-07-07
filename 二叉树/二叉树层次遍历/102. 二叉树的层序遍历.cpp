4d162b7e94e59aea23a5b2f84d42a121a49ec497
Don't bully Miyu <141989008+WhereIsYuuka@users.noreply.github.com>
2025-07-07 11:48:11 +0800

    2025/7/7 102. 二叉树的层序遍历

--- 二叉树/二叉树层次遍历/102. 二叉树的层序遍历.cpp
+++ 二叉树/二叉树层次遍历/102. 二叉树的层序遍历.cpp
@@ -36,3 +36,42 @@ public:
         return res;
     }
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+// class Solution {
+// public:
+//     vector<vector<int>> levelOrder(TreeNode* root) {
+//         vector<vector<int>> res;
+//         if(!root)
+//             return res;
+//         queue<TreeNode*> treeQue;
+//         treeQue.emplace(root);
+//         while(!treeQue.empty())
+//         {
+//             vector<int> rowTree;
+//             int len = treeQue.size();
+//             for(int i = 0; i < len; i++)
+//             {
+//                 TreeNode* tmp = treeQue.front();
+//                 rowTree.emplace_back(tmp->val);
+//                 treeQue.pop();
+//                 if(tmp->left)
+//                     treeQue.emplace(tmp->left);
+//                 if(tmp->right)
+//                     treeQue.emplace(tmp->right);
+//             }
+//             res.emplace_back(rowTree);
+//         }
+//         return res;
+//     }
+// };
