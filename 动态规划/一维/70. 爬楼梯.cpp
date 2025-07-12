ad441f7f6585f8353e45b197d41b81aeabaeacde
Don't bully Miyu <141989008+WhereIsYuuka@users.noreply.github.com>
2025-07-12 21:51:09 +0800

    2025/7/12 70. 爬楼梯

--- 动态规划/一维/70. 爬楼梯.cpp
+++ 动态规划/一维/70. 爬楼梯.cpp
@@ -21,3 +21,18 @@ public:
 //         return climbStairs(n-1) + climbStairs(n-2);
 //     }
 // };
+
+// class Solution {
+// public:
+//     int climbStairs(int n) {
+//         if(n <= 3)
+//             return n;
+//         int a = 1, b = 2;
+//         for(int i = 2; i < n; i++)
+//         {
+//             b = b + a;
+//             a = b - a;
+//         }
+//         return b;
+//     }
+// };
