5647c25e5ee5bcbba39322061e774f4e383ab9ab
Don't bully Miyu <141989008+WhereIsYuuka@users.noreply.github.com>
2025-07-13 12:59:45 +0800

    2025/7/13 124. 二叉树中的最大路径和

--- 二叉树/二叉树/124. 二叉树中的最大路径和.cpp
+++ 二叉树/二叉树/124. 二叉树中的最大路径和.cpp
@@ -47,3 +47,35 @@ public:
 //         return max(max(l, r) + root->val, 0);
 //     }
 // };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+// class Solution {
+// private:
+//     int numMax = INT_MIN;
+
+// public:
+//     int maxPathSum(TreeNode* root) {
+//         countMax(root);
+//         return numMax;
+//     }
+
+//     int countMax(TreeNode* root)
+//     {
+//         if(!root)
+//             return 0;
+//         int left = max(countMax(root->left), 0);
+//         int right = max(countMax(root->right), 0);
+//         numMax = max(numMax, root->val + left + right);
+//         return root->val + max(left, right);
+//     }
+// };
