01aa5d15d3dd6521668c78c5f2e716470bea7fb6
Don't bully Miyu <141989008+WhereIsYuuka@users.noreply.github.com>
2025-07-14 15:26:04 +0800

    2025/7/14 76. 最小覆盖子串

--- 滑动窗口/76. 最小覆盖子串.cpp
+++ 滑动窗口/76. 最小覆盖子串.cpp
@@ -37,3 +37,45 @@ public:
         return len == INT_MAX ? "" : s.substr(start, len);
     }
 };
+
+// class Solution {
+// public:
+//     string minWindow(string s, string t) {
+//         if(s.length() < t.length())
+//             return "";
+//         unordered_map<char, int> cnt;
+//         int l = 0, len = s.length(), loss = 0;
+//         int lRes = -1, rRes = len;
+//         for(auto ch : t)
+//         {
+//             if(cnt[ch] == 0)
+//                 loss++;
+//             cnt[ch]++;
+//         }
+//         for(int r = 0; r < len; r++)
+//         {
+//             char ch = s[r];
+//             cnt[ch]--;
+//             if(cnt[ch] == 0)
+//             {
+//                 loss--;
+//             }
+//             while(loss == 0)
+//             {
+//                 if(r - l < rRes - lRes)
+//                 {
+//                     rRes = r;
+//                     lRes = l;
+//                 }
+//                 char chL = s[l];
+//                 if(cnt[chL] == 0)
+//                 {
+//                     loss++;
+//                 }
+//                 cnt[chL]++;
+//                 l++;
+//             }
+//         }
+//         return lRes < 0 ? "" : s.substr(lRes, rRes - lRes + 1);
+//     }
+// };
