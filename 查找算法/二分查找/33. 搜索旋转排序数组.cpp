c8400bbd9d5d2d0bc84bd0305ffa365292d5ea59
Don't bully Miyu <141989008+WhereIsYuuka@users.noreply.github.com>
2025-07-17 17:30:21 +0800

    2025/7/17 33. 搜索旋转排序数组

--- 查找算法/二分查找/33. 搜索旋转排序数组.cpp
+++ 查找算法/二分查找/33. 搜索旋转排序数组.cpp
@@ -21,3 +21,35 @@ public:
         return -1;
     }
 };
+
+// class Solution {
+// public:
+//     int search(vector<int>& nums, int target) {
+//         int len = nums.size();
+//         if(len == 0)
+//             return -1;
+//         int l = 0, r = len -1;
+//         while(l < r)
+//         {
+//             int mid = l + (r - l) / 2;
+//             if(nums[mid] > nums[r])
+//                 l = mid + 1;
+//             else
+//                 r = mid;
+//         }
+//         int offset = l;
+//         l = 0, r = len - 1;
+//         while(l <= r)
+//         {
+//             int mid = l + (r - l) / 2;
+//             int relMid = (mid + offset) % len;
+//             if(nums[relMid] == target)
+//                 return relMid;
+//             if(nums[relMid] < target)
+//                 l = mid + 1;
+//             else
+//                 r = mid - 1;
+//         }
+//         return -1;
+//     }
+// };
