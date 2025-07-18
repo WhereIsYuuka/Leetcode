6b465ae9d207f026f2f2b9b67321b22588a16ce2
Don't bully Miyu <141989008+WhereIsYuuka@users.noreply.github.com>
2025-07-18 21:44:32 +0800

    2025/7/18 79. 单词搜索

--- 回溯/79. 单词搜索.cpp
+++ 回溯/79. 单词搜索.cpp
@@ -34,3 +34,44 @@ public:
         return false;
     }
 };
+
+// class Solution {
+// public:
+//     int dx[4] = {-1, 0, 1, 0}, dy[4] = {0, -1, 0, 1};
+//     bool exist(vector<vector<char>>& board, string word) {
+//         for(int i = 0; i < board.size(); i++)
+//         {
+//             for(int j = 0; j < board[0].size(); j++)
+//             {
+//                 if(dfs(board, word, i, j, 0))
+//                     return true;
+//             }
+//         }
+//         return false;
+//     }
+
+//     bool dfs(vector<vector<char>>& board, string word, int x, int y, int idx)
+//     {
+//         if(word[idx] != board[x][y])
+//             return false;
+//         if(idx == word.length() - 1)
+//             return true;
+//         char ch = board[x][y];
+//         board[x][y] = '.';
+//         for(int i = 0; i < 4; i++)
+//         {
+//             int nx = x + dx[i];
+//             int ny = y + dy[i];
+//             if(nx < 0 || nx >= board.size() || ny < 0 || ny >= board[0].size())
+//             {
+//                 continue;
+//             }
+//             if(dfs(board, word, nx, ny, idx+1))
+//             {
+//                 return true;
+//             }
+//         }
+//         board[x][y] = ch;
+//         return false;
+//     }
+// };
