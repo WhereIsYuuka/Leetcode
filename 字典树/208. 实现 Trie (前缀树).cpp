f47966c19b842c78eb006f485f921acae3fb7da5
Don't bully Miyu <141989008+WhereIsYuuka@users.noreply.github.com>
2025-07-21 18:51:50 +0800

    2025/7/21 208. 实现 Trie (前缀树)   0721 Ciallo～(∠・ω< )⌒☆

--- 字典树/208. 实现 Trie (前缀树).cpp
+++ 字典树/208. 实现 Trie (前缀树).cpp
@@ -54,3 +54,64 @@ class Trie {
      * bool param_2 = obj->search(word);
      * bool param_3 = obj->startsWith(prefix);
      */
+
+
+// class Trie {
+// private:
+//     struct Node{
+//         bool isEnd;
+//         Node *child[26]{};
+//     };
+
+// public:
+//     Node *root = new Node();
+
+//     Trie() {
+        
+//     }
+    
+//     void insert(string word) {
+//         Node *node = root;
+
+//         for(auto ch : word)
+//         {
+//             int idx = ch - 'a';
+//             if(node->child[idx] == nullptr)
+//             {
+//                 node->child[idx] = new Node();
+//             }
+//             node = node->child[idx];
+//         }
+//         node->isEnd = true;
+//     }
+    
+//     bool search(string word) {
+//         return Search(word) == 1;
+//     }
+    
+//     bool startsWith(string prefix) {
+//         return Search(prefix) != -1;
+//     }
+
+//     int Search(string word)
+//     {
+//         Node *node = root;
+
+//         for(auto ch : word)
+//         {
+//             int idx = ch - 'a';
+//             if(node->child[idx] == nullptr)
+//                 return -1;
+//             node = node->child[idx];
+//         }
+//         return node->isEnd ? 1 : 0;
+//     }
+// };
+
+// /**
+//  * Your Trie object will be instantiated and called as such:
+//  * Trie* obj = new Trie();
+//  * obj->insert(word);
+//  * bool param_2 = obj->search(word);
+//  * bool param_3 = obj->startsWith(prefix);
+//  */
