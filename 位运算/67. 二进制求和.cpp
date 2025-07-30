78bbe239da073aecf2f7776228be083e93cb636a
Yusee <caoniou@gmail.com>
2025-07-30 21:37:34 +0800

    2025/7/30 67. 二进制求和

--- 位运算/67. 二进制求和.cpp
+++ 位运算/67. 二进制求和.cpp
@@ -14,3 +14,23 @@ public:
         return res;
     }
 };
+
+
+// class Solution {
+// public:
+//     string addBinary(string a, string b) {
+//         string res;
+//         int tmp = 0;
+//         int i = a.length()-1, j = b.length()-1;
+//         while(i >= 0 || j >= 0 || tmp)
+//         {
+//             if(i >= 0)
+//                 tmp += a[i--] - '0';
+//             if(j >= 0)
+//                 tmp += b[j--] - '0';
+//             res = to_string(tmp % 2) + res;
+//             tmp >>= 1;
+//         }
+//         return res;
+//     }
+// };
