63f60117a1486d50ab776d4e6dd0143de443480a
Yusee <caoniou@gmail.com>
2025-08-03 21:39:31 +0800

    2025/8/3 5. 最长回文子串

--- 动态规划/多维/5. 最长回文子串.cpp
+++ 动态规划/多维/5. 最长回文子串.cpp
@@ -22,3 +22,26 @@ public:
         return res;
     }
 };
+
+// class Solution {
+// public:
+//     string longestPalindrome(string s) {
+//         string res = "";
+//         int n = s.length();
+//         int l = 0, r = 0;
+//         for(int i = 0; i < n; i++)
+//         {
+//             l = i-1, r = i+1;
+//             while(l >= 0 && r < n && s[l] == s[r])
+//                 r++, l--;
+//             if(res.length() < r - l - 1)
+//                 res = s.substr(l + 1, r - l - 1);
+//             l = i, r = i+1;
+//             while(l >= 0 && r < n && s[l] == s[r])
+//                 r++, l--;
+//             if(res.length() < r - l - 1)
+//                 res = s.substr(l + 1, r - l - 1);
+//         }
+//         return res;
+//     }
+// };
