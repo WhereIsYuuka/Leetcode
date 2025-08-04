50e553937c945fe78470837ae1f4ec81811b39c8
Yusee <caoniou@gmail.com>
2025-08-04 22:17:06 +0800

    2025/8/4 236. 二叉树的最近公共祖先

--- 二叉树/二叉树/236. 二叉树的最近公共祖先.cpp
+++ 二叉树/二叉树/236. 二叉树的最近公共祖先.cpp
@@ -11,3 +11,27 @@ public:
             return root;
         return left ? left : right;
 };
+
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+//         if(!root)
+//             return NULL;
+//         if(root == p || root == q)
+//             return root;
+//         TreeNode* left = lowestCommonAncestor(root->left, p, q);
+//         TreeNode* right = lowestCommonAncestor(root->right, p, q);
+//         if(left && right)
+//             return root;
+//         return left == NULL ? right : left;
+//     }
+// };
