6521ac7800eef16c44275d764bec0f5ecac7d057
Yusee <caoniou@gmail.com>
2025-08-06 10:50:08 +0800

    2025/8/6 300. 最长递增子序列

--- 动态规划/一维/300. 最长递增子序列.cpp
+++ 动态规划/一维/300. 最长递增子序列.cpp
@@ -14,3 +14,24 @@ public:
         return res;
     }
 };
+
+// class Solution {
+// public:
+//     int lengthOfLIS(vector<int>& nums) {
+//         int n = nums.size(), res = 0;
+//         if(n < 1)
+//             return res;
+//         vector<int> dp(n, 1);
+//         for(int i = 0; i < n; i++)
+//         {
+//             for(int j = 0; j < i; j++)
+//             {
+//                 if(nums[j] < nums[i])
+//                     dp[i] = max(dp[j] + 1, dp[i]);
+//             }
+//             res = max(res, dp[i]);
+//         }
+//         return res;
+//     }
+// };
+
