60c07dc7a46bc7c2e3f86661c367a1796a8917f3
Yusee <caoniou@gmail.com>
2025-08-07 10:31:18 +0800

    2025/8/7 230. 二叉搜索树中第 K 小的元素

--- 二叉树/二叉搜索树/230. 二叉搜索树中第K小的元素.cpp
+++ 二叉树/二叉搜索树/230. 二叉搜索树中第K小的元素.cpp
@@ -62,3 +62,35 @@ public:
 //         return nums[k-1];
 //     }
 // };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+// class Solution {
+// public:
+//     int kthSmallest(TreeNode* root, int k) {
+//         int res = -1;
+
+//         auto dfs = [&](this auto&& dfs, TreeNode* root){
+//             if(root == nullptr)
+//                 return;
+//             dfs(root->left);
+//             if(--k == 0)
+//             {
+//                 res = root->val;
+//                 return;
+//             }
+//             dfs(root->right);
+//         };
+//         dfs(root);
+//         return res;
+//     }
+// };
