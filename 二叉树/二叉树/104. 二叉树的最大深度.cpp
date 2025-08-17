c84a7824777ed9b24c2423aafecf21dda3944562
Yusee <caoniou@gmail.com>
2025-08-17 23:41:28 +0800

    2025/8/17 104. 二叉树的最大深度

--- 二叉树/二叉树/104. 二叉树的最大深度.cpp
+++ 二叉树/二叉树/104. 二叉树的最大深度.cpp
@@ -27,3 +27,36 @@ public:
         return res;
     }
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+// class Solution {
+// private:
+//     int res = 0;
+
+// public:
+//     int maxDepth(TreeNode* root) {
+//         if(!root)
+//             return 0;
+        
+//         auto dfs = [&](this auto&& dfs, TreeNode* root, int i) -> void{
+//             if(!root)
+//                 return;
+//             res = max(res, i);
+//             dfs(root->left, i+1);
+//             dfs(root->right, i+1);
+//         };
+
+//         dfs(root, 1);
+//         return res;
+//     }
+// };
