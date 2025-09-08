8336387af97b9a8f10b8292af78c9d2efa2de3d8
Yusee <caoniou@gmail.com>
2025-09-09 00:00:40 +0800

    2025/9/8 74. 搜索二维矩阵

--- 查找算法/二分查找/74. 搜索二维矩阵.cpp
+++ 查找算法/二分查找/74. 搜索二维矩阵.cpp
@@ -22,3 +22,24 @@ public:
         return false;
     }
 };
+
+// class Solution {
+// public:
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+//         int n = matrix.size(), m = matrix[0].size();
+//         int l = 0, r = matrix.size() * matrix[0].size() - 1;
+//         int mid, tmp;
+//         while(l <= r)
+//         {
+//             mid = l + (r - l) / 2;
+//             tmp = matrix[mid / m][mid % m];
+//             if(target == tmp)
+//                 return true;
+//             else if(target < tmp)
+//                 r = mid - 1;
+//             else
+//                 l = mid + 1; 
+//         }
+//         return false;
+//     }
+// };
