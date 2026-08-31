b487bd93580942622262c34bb880ff3de58cb32c
Yusee <caoniou@gmail.com>
2026-08-31 11:13:35 +0800

    2026/8/31

--- 矩阵/73. 矩阵置零.cpp
+++ 矩阵/73. 矩阵置零.cpp
@@ -1,70 +1,105 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        bool firstRow = false, firstCol = false;
-        for(int i = 0; i < matrix.size(); i++)
+        int n = matrix.size(), m = matrix[0].size();
+        vector<int> x(m, 0), y(n, 0);
+        for(int i = 0; i < n; i++)
         {
-            if(matrix[i][0] == 0)
-            {
-                firstCol = true;
-                break;
-            }
-        }
-        for(int i = 0; i < matrix[0].size(); i++)
-        {
-            if(matrix[0][i] == 0)
-            {
-                firstRow = true;
-                break;
-            }
-        }
-        for(int i = 1; i < matrix.size(); i++)
-        {
-            for(int j = 1; j < matrix[0].size(); j++)
+            for(int j = 0; j < m; j++)
             {
                 if(matrix[i][j] == 0)
                 {
-                    matrix[i][0] = 0;
-                    matrix[0][j] = 0; 
+                    x[j] = 1;
+                    y[i] = 1;
                 }
             }
         }
-        for(int i = 1; i < matrix.size(); i++)
+        for(int i = 0; i < m; i++)
         {
-            if(matrix[i][0] == 0)
+            if(x[i] == 1)
             {
-                for(int j = 1; j < matrix[0].size(); j++)
-                {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-        for(int i = 1; i < matrix[0].size(); i++)
-        {
-            if(matrix[0][i] == 0)
-            {
-                for(int j = 1; j < matrix.size(); j++)
-                {
+                for(int j = 0; j < n; j++)
                     matrix[j][i] = 0;
-                }
             }
         }
-        if(firstRow)
+        for(int i = 0; i < n; i++)
         {
-            for(int i = 0; i < matrix[0].size(); i++)
+            if(y[i] == 1)
             {
-                matrix[0][i] = 0;
-            }
-        }
-        if(firstCol)
-        {
-            for(int i = 0; i < matrix.size(); i++)
-            {
-                matrix[i][0] = 0;
+                for(int j = 0; j < m; j++)
+                    matrix[i][j] = 0;
             }
         }
     }
 };
+
+// class Solution {
+// public:
+//     void setZeroes(vector<vector<int>>& matrix) {
+//         bool firstRow = false, firstCol = false;
+//         for(int i = 0; i < matrix.size(); i++)
+//         {
+//             if(matrix[i][0] == 0)
+//             {
+//                 firstCol = true;
+//                 break;
+//             }
+//         }
+//         for(int i = 0; i < matrix[0].size(); i++)
+//         {
+//             if(matrix[0][i] == 0)
+//             {
+//                 firstRow = true;
+//                 break;
+//             }
+//         }
+//         for(int i = 1; i < matrix.size(); i++)
+//         {
+//             for(int j = 1; j < matrix[0].size(); j++)
+//             {
+//                 if(matrix[i][j] == 0)
+//                 {
+//                     matrix[i][0] = 0;
+//                     matrix[0][j] = 0; 
+//                 }
+//             }
+//         }
+//         for(int i = 1; i < matrix.size(); i++)
+//         {
+//             if(matrix[i][0] == 0)
+//             {
+//                 for(int j = 1; j < matrix[0].size(); j++)
+//                 {
+//                     matrix[i][j] = 0;
+//                 }
+//             }
+//         }
+//         for(int i = 1; i < matrix[0].size(); i++)
+//         {
+//             if(matrix[0][i] == 0)
+//             {
+//                 for(int j = 1; j < matrix.size(); j++)
+//                 {
+//                     matrix[j][i] = 0;
+//                 }
+//             }
+//         }
+//         if(firstRow)
+//         {
+//             for(int i = 0; i < matrix[0].size(); i++)
+//             {
+//                 matrix[0][i] = 0;
+//             }
+//         }
+//         if(firstCol)
+//         {
+//             for(int i = 0; i < matrix.size(); i++)
+//             {
+//                 matrix[i][0] = 0;
+//             }
+//         }
+//     }
+// };
 
 // class Solution {
 // public:
