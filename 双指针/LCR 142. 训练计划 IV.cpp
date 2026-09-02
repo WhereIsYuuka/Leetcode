2f4a6bb0a907e118f303d311753f02eee0019445
Yusee <caoniou@gmail.com>
2026-09-02 11:11:15 +0800

    2026/9/2

--- 双指针/LCR 142. 训练计划 IV.cpp
+++ 双指针/LCR 142. 训练计划 IV.cpp
@@ -8,30 +8,60 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
-    public:
-        ListNode* trainningPlan(ListNode* l1, ListNode* l2) {
-            ListNode* res = new ListNode(0);
-            ListNode* tmp = res;
-            while(l1 != nullptr && l2 != nullptr)
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode *slow = head, *fast = head;
+        while(fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast)
             {
-                if(l1->val <= l2->val)
+                while(head != slow)
                 {
-                    tmp->next = l1;
-                    l1 = l1->next;
+                    head = head->next;
+                    slow = slow->next;
                 }
-                else
-                {
-                    tmp->next = l2;
-                    l2 = l2->next;
-                }
-                tmp = tmp->next;
+                return slow;
             }
-            if(l1 == nullptr)
-                tmp->next = l2;
-            if(l2 == nullptr)
-                tmp->next = l1;
-    
-            return res->next;
         }
-    };
+        return NULL;
+    }
+};
+
+// class Solution {
+//     public:
+//         ListNode* trainningPlan(ListNode* l1, ListNode* l2) {
+//             ListNode* res = new ListNode(0);
+//             ListNode* tmp = res;
+//             while(l1 != nullptr && l2 != nullptr)
+//             {
+//                 if(l1->val <= l2->val)
+//                 {
+//                     tmp->next = l1;
+//                     l1 = l1->next;
+//                 }
+//                 else
+//                 {
+//                     tmp->next = l2;
+//                     l2 = l2->next;
+//                 }
+//                 tmp = tmp->next;
+//             }
+//             if(l1 == nullptr)
+//                 tmp->next = l2;
+//             if(l2 == nullptr)
+//                 tmp->next = l1;
+    
+//             return res->next;
+//         }
+//     };
