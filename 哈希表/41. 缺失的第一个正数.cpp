45eac3a0604b1e7d224db8fff9a3c8f6d064bc33
Yusee <caoniou@gmail.com>
2026-09-09 23:16:53 +0800

    2026/9/9 41. 缺失的第一个正数

---
+++ 哈希表/41. 缺失的第一个正数.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        for(int i = 0; i < n; i++)
+        {
+            while(nums[i] <= n && nums[i] > 0 && nums[i] != nums[nums[i]-1])
+            {
+                int tmp = nums[i] - 1;
+                swap(nums[i], nums[tmp]);
+            }
+        }
+        for(int i = 0; i < n; i++)
+        {
+            if(nums[i] != i+1)
+                return i+1;
+        }
+        return n+1;
+    }
+};
