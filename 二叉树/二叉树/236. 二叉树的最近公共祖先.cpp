0a887323063a021c17b32af1444811dcc81c1b2e
Yusee <caoniou@gmail.com>
2026-09-15 14:19:29 +0800

    2026/9/15 236. 二叉树的最近公共祖先

--- 二叉树/二叉树/236. 二叉树的最近公共祖先.cpp
+++ 二叉树/二叉树/236. 二叉树的最近公共祖先.cpp
@@ -1,16 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(!root)
-            return nullptr;
+        if(root == NULL)
+            return NULL;
         if(root == p || root == q)
             return root;
-        TreeNode* left = lowestCommonAncestor(root->left, p, q);
-        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        TreeNode *left, *right;
+        left = lowestCommonAncestor(root->left, p, q);
+        right = lowestCommonAncestor(root->right, p, q);
+
+        if(left == NULL)
+            return right;
+        if(right == NULL)
+            return left;
         if(left && right)
             return root;
-        return left ? left : right;
+        return NULL;
+    }
 };
+
+// class Solution {
+// public:
+//     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+//         if(!root)
+//             return nullptr;
+//         if(root == p || root == q)
+//             return root;
+//         TreeNode* left = lowestCommonAncestor(root->left, p, q);
+//         TreeNode* right = lowestCommonAncestor(root->right, p, q);
+//         if(left && right)
+//             return root;
+//         return left ? left : right;
+// };
 
 // /**
 //  * Definition for a binary tree node.
