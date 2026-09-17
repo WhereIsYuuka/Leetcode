0e249625f6bb4b99b690650e83c03aca66e10889
Yusee <caoniou@gmail.com>
2026-09-17 19:52:48 +0800

    2026/9/17 139. 单词拆分

--- 动态规划/一维/139. 单词拆分.cpp
+++ 动态规划/一维/139. 单词拆分.cpp
@@ -1,21 +1,41 @@
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_set<string> dict(wordDict.begin(), wordDict.end());
-        int len = s.size();
-        vector<short> dp(len + 1, 0);
-        dp[0] = 1;
-        for(int i = 1; i <= len; i++)
+        int n = s.length();
+        vector<bool> dp(n+1, false);
+        dp[0] = true;
+        for(int i = 0; i < n; i++)
         {
-            for(int j = 0; j < i; j++)
+            if(!dp[i])
+                continue;
+            for(auto& it : wordDict)
             {
-                if(dp[j] && dict.find(s.substr(j, i-j)) != dict.end())   //dp[j]表示前j个字符是否可以拆分
-                {
-                    dp[i] = 1;
-                    break;
-                }
+                if(it.length() + i <= n && s.substr(i, it.length()) == it)
+                    dp[i + it.length()] = true;
             }
         }
-        return dp[len] ? true : false;
+        return dp[n];
     }
 };
+
+// class Solution {
+// public:
+//     bool wordBreak(string s, vector<string>& wordDict) {
+//         unordered_set<string> dict(wordDict.begin(), wordDict.end());
+//         int len = s.size();
+//         vector<short> dp(len + 1, 0);
+//         dp[0] = 1;
+//         for(int i = 1; i <= len; i++)
+//         {
+//             for(int j = 0; j < i; j++)
+//             {
+//                 if(dp[j] && dict.find(s.substr(j, i-j)) != dict.end())   //dp[j]表示前j个字符是否可以拆分
+//                 {
+//                     dp[i] = 1;
+//                     break;
+//                 }
+//             }
+//         }
+//         return dp[len] ? true : false;
+//     }
+// };
