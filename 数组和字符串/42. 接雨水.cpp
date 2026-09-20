914b130ace24a319e6f2a2ee46341ada78dce4e9
Yusee <caoniou@gmail.com>
2026-09-20 20:08:15 +0800

    2026/9/20 42. 接雨水

--- 数组和字符串/42. 接雨水.cpp
+++ 数组和字符串/42. 接雨水.cpp
@@ -1,23 +1,49 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int l = 0, r = height.size() - 1;
-        int lMax = 0, rMax = 0 ,res = 0;
+        int n =height.size();
+        int l = 0, r = n-1;
+        int res = 0;
+        int lMax = 0, rMax = 0;
         while(l < r)
         {
             lMax = max(lMax, height[l]);
             rMax = max(rMax, height[r]);
             if(lMax < rMax)
             {
-                res += (lMax - height[l]);
+                res += lMax - height[l];
                 l++;
             }
             else
             {
-                res += (rMax - height[r]);
+                res += rMax - height[r];
                 r--;
             }
         }
         return res;
     }
 };
+
+// class Solution {
+// public:
+//     int trap(vector<int>& height) {
+//         int l = 0, r = height.size() - 1;
+//         int lMax = 0, rMax = 0 ,res = 0;
+//         while(l < r)
+//         {
+//             lMax = max(lMax, height[l]);
+//             rMax = max(rMax, height[r]);
+//             if(lMax < rMax)
+//             {
+//                 res += (lMax - height[l]);
+//                 l++;
+//             }
+//             else
+//             {
+//                 res += (rMax - height[r]);
+//                 r--;
+//             }
+//         }
+//         return res;
+//     }
+// };
