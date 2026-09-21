978e4b66b86ccf17af479cd8227bb426ed66a2ab
Yusee <caoniou@gmail.com>
2026-09-21 10:20:44 +0800

    2026/9/21 104. 二叉树的最大深度

--- 二叉树/二叉树/104. 二叉树的最大深度.cpp
+++ 二叉树/二叉树/104. 二叉树的最大深度.cpp
@@ -11,34 +11,36 @@
  */
 class Solution {
 public:
-    int res = 0;
-
-    void dfs(TreeNode* root, int depth) 
-    {
-        if(root == nullptr)
-            return;
-        res = max(res, depth + 1);
-        dfs(root->left, depth + 1);
-        dfs(root->right, depth + 1);
-    }
-
     int maxDepth(TreeNode* root) {
-        dfs(root, 0);
-        return res;
+        if(root == nullptr)
+            return 0;
+        int left = maxDepth(root->left);
+        int right = maxDepth(root->right);
+
+        return max(left, right) + 1;
     }
 };
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// class Solution {
+// public:
+//     int res = 0;
+
+//     void dfs(TreeNode* root, int depth) 
+//     {
+//         if(root == nullptr)
+//             return;
+//         res = max(res, depth + 1);
+//         dfs(root->left, depth + 1);
+//         dfs(root->right, depth + 1);
+//     }
+
+//     int maxDepth(TreeNode* root) {
+//         dfs(root, 0);
+//         return res;
+//     }
+// };
+
+
 // class Solution {
 // private:
 //     int res = 0;
