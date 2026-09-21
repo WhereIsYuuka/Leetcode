0c2054897a52a4325741a4f25bdcb6aa3a62d692
Yusee <caoniou@gmail.com>
2026-09-21 09:58:10 +0800

    2026/9/21 5. 最长回文子串

--- 动态规划/多维/5. 最长回文子串.cpp
+++ 动态规划/多维/5. 最长回文子串.cpp
@@ -1,27 +1,66 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        string res = "";
         int n = s.length();
-
+        if(n < 1)
+            return "";
+        int left = 0, right = 0, len = 1;
+        int maxStart, maxLen = 0;
         for(int i = 0; i < n; i++)
         {
-            //奇数
-            int l = i-1, r = i+1;
-            while(l >= 0 && r < n && s[l] == s[r])
-                l--, r++;
-            if(res.length() < r-l-1)
-                res = s.substr(l+1, r-l-1);
-            //偶数
-            l = i, r = i+1;
-            while(l >= 0 && r < n && s[l] == s[r])
-                l--, r++;
-            if(res.length() < r-l-1)
-                res = s.substr(l+1, r-l-1);
+            left = i-1, right = i+1;
+            while(left >= 0 && s[left] == s[i])
+            {
+                left--;
+                len++;
+            }
+            while(right < n && s[right] == s[i])
+            {
+                right++;
+                len++;
+            }
+            while(left >= 0 && right < n && s[right] == s[left])
+            {
+                left--;
+                right++;
+                len += 2;
+            }
+            if(len > maxLen)
+            {
+                maxLen = len;
+                maxStart = left;
+            }
+            len = 1;
+
         }
-        return res;
+        return s.substr(maxStart+1, maxLen);
     }
 };
+
+// class Solution {
+// public:
+//     string longestPalindrome(string s) {
+//         string res = "";
+//         int n = s.length();
+
+//         for(int i = 0; i < n; i++)
+//         {
+//             //奇数
+//             int l = i-1, r = i+1;
+//             while(l >= 0 && r < n && s[l] == s[r])
+//                 l--, r++;
+//             if(res.length() < r-l-1)
+//                 res = s.substr(l+1, r-l-1);
+//             //偶数
+//             l = i, r = i+1;
+//             while(l >= 0 && r < n && s[l] == s[r])
+//                 l--, r++;
+//             if(res.length() < r-l-1)
+//                 res = s.substr(l+1, r-l-1);
+//         }
+//         return res;
+//     }
+// };
 
 // class Solution {
 // public:
