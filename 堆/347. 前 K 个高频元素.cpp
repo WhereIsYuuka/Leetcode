dc873a425680049ace229318e76bd4d44210fbe5
Yusee <caoniou@gmail.com>
2026-09-23 10:55:10 +0800

    2026/9/23 347. 前 K 个高频元素

--- 堆/347. 前 K 个高频元素.cpp
+++ 堆/347. 前 K 个高频元素.cpp
@@ -1,24 +1,55 @@
 class Solution {
 public:
-    //Count the number of occurrences and get it from high to low
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int, int> cnt;
+        unordered_map<int, int> ump;
         int maxCnt = 0;
-        for(auto num : nums)
+        for(auto it : nums)
         {
-            cnt[num]++;
-            maxCnt = max(maxCnt, cnt[num]);
+            ump[it]++;
+            maxCnt = max(maxCnt, ump[it]);
         }
-        vector<vector<int>> rev(maxCnt + 1);
-        for(auto [a, b] : cnt)
+        vector<vector<int>> buckets(maxCnt + 1);
+        for(auto& [num, time] : ump)
         {
-            rev[b].emplace_back(a);
+            buckets[time].emplace_back(num);
         }
         vector<int> res;
-        for(int i = maxCnt; i >= 0 && res.size() < k; i--)
+        int cnt = 0;
+        for(int i = maxCnt; i >= 0; i--)
         {
-            res.insert(res.end(), rev[i].begin(), rev[i].end());
+            for(auto it : buckets[i])
+            {
+                res.emplace_back(it);
+                cnt++;
+                if(cnt == k)
+                    return res;
+            }
         }
         return res;
     }
 };
+
+// class Solution {
+// public:
+//     //Count the number of occurrences and get it from high to low
+//     vector<int> topKFrequent(vector<int>& nums, int k) {
+//         unordered_map<int, int> cnt;
+//         int maxCnt = 0;
+//         for(auto num : nums)
+//         {
+//             cnt[num]++;
+//             maxCnt = max(maxCnt, cnt[num]);
+//         }
+//         vector<vector<int>> rev(maxCnt + 1);
+//         for(auto [a, b] : cnt)
+//         {
+//             rev[b].emplace_back(a);
+//         }
+//         vector<int> res;
+//         for(int i = maxCnt; i >= 0 && res.size() < k; i--)
+//         {
+//             res.insert(res.end(), rev[i].begin(), rev[i].end());
+//         }
+//         return res;
+//     }
+// };
